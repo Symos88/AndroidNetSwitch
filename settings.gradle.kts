88770e569b57c5376f902af14bef724f377e2999
Symos88 <symos1988@gmail.com>
2026-08-06 12:28:46 +0200

    fix: resolve merge conflict in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,10 +5,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> 4796efa6d360366945c93e5b3f35e036dc81a035
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -16,9 +13,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> 4796efa6d360366945c93e5b3f35e036dc81a035
 rootProject.name = "NetSwitch"
 include(":app")
