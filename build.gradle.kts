690fb1acd9b44fac0ee2166c1a1e8740fd80ad22
Symos88 <symos1988@gmail.com>
2026-08-06 12:41:37 +0200

    fix: force-clean build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,5 @@
-// Top-level build file
 plugins {
-<<<<<<< HEAD
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
-=======
-    id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
->>>>>>> 4796efa6d360366945c93e5b3f35e036dc81a035
 }
