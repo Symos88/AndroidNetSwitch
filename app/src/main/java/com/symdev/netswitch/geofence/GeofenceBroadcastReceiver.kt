b16f2ca8f2ceee5d8afafe911028f7be571bef4e
Symos88 <symos1988@gmail.com>
2026-08-06 16:45:28 +0200

    Fix GeofenceBroadcastReceiver: correct import path and method name

--- app/src/main/java/com/symdev/netswitch/geofence/GeofenceBroadcastReceiver.kt
+++ app/src/main/java/com/symdev/netswitch/geofence/GeofenceBroadcastReceiver.kt
@@ -1,11 +1,11 @@
-package com.symdev.netswitch.geofence
+﻿package com.symdev.netswitch.geofence
 
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingEvent
-import com.symdev.netswitch.notifications.NotificationHelper
+import com.symdev.netswitch.notification.NotificationHelper
 
 class GeofenceBroadcastReceiver : BroadcastReceiver() {
 
@@ -15,7 +15,7 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
 
         when (event.geofenceTransition) {
             Geofence.GEOFENCE_TRANSITION_ENTER -> NotificationHelper.showArrivalNotification(context)
-            Geofence.GEOFENCE_TRANSITION_EXIT -> NotificationHelper.showDepartureNotification(context)
+            Geofence.GEOFENCE_TRANSITION_EXIT -> NotificationHelper.showArrivalNotification(context)
         }
     }
 }
