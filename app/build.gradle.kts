205bad453d7fb925049074c25e3d88f712aa68fa
Symos88 <symos1988@gmail.com>
2026-08-09 02:15:46 +0200

    build: target Android 16 API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.symdev.netswitch"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.symdev.netswitch"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
